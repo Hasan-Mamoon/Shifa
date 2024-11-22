871e167761d00257b47ae87feedd8a74c916b4a3
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-11-22 21:24:29 +0500

    create the basic structure and tags of Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+
+
+<div>
+    <img src={assets.logo} alt="" />
+    <p>"Shifa - Your Trusted Partner in Health and Wellness"
+    We’re committed to connecting you with qualified doctors and healthcare services to ensure your well-being is always a priority. Book appointments, access preventive health plans, and manage your medical needs seamlessly—all in one place.</p>
+</div>
+
+
+
+
+<div>
+    COMPANY
+    <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact us</li>
+        
+    </ul>
+</div>
+
+
+
+
+<div>
+    <p>GET IN TOUCH</p>
+    <li>f219418@cfd.nu.edu.pk</li>
+    <li>f219414@cfd.nu.edu.pk</li>
+    <li>f219444@cfd.nu.edu.pk</li>
+</div>
+
+
+
+
+
+      </div>
+
+      <div>
+        <hr />
+        <p>Copyright 2024 SHIFA - All Right Reserved.</p>
+      </div>
     </div>
   )
 }
