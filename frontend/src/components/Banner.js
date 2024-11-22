688134ce90cc92f594357bb26dad5329275271d4
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-11-22 20:58:01 +0500

    created the basic required tags in banner component

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Banner = () => {
   return (
+  <div>
     <div>
-      
+<div>
+    <p>Book Appointment</p>
+    <p>With 100+ Trusted Doctors</p>
+    <button>Create account</button>
+</div>
     </div>
+
+
+
+
+
+
+
+    <div>
+<img src={assets.appointment_img} alt="" />
+    </div>
+  </div>
+
   )
 }
 
