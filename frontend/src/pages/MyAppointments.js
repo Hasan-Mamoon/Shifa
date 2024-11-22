c68619a3551652475289a2ba51bd9942e1c000a0
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-11-23 04:12:56 +0500

    create the complete structure of MyAppointments page

--- frontend/src/pages/MyAppointments.js
+++ frontend/src/pages/MyAppointments.js
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {AppContext} from '../context/AppContext'
 const MyAppointments = () => {
+
+  const {doctors}=useContext(AppContext)
   return (
     <div>
-      
+      <p >My Appointments</p>
+      <div>
+{doctors.slice(0,2).map((item,index)=>(
+  <div key={index}>
+    <div>
+      <img src={item.image} alt="" />
+    </div>
+    <div>
+      <p>{item.name}</p>
+      <p>{item.speciality}</p>
+      <p>Address:</p>
+      <p>{item.address.line1}</p>
+      <p>{item.address.line2}</p>
+      <p><span>Date & Time:</span></p>
+    </div>
+<div></div>
+<div>
+  <button>Pay Online</button>
+  <button>Cancel appointment</button>
+</div>
+  </div>
+))}
+      </div>
     </div>
   )
 }
