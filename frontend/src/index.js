f9f1cd7ab0e1767d55d2fed8ca99d4a4608eaed6
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-11-22 20:03:13 +0500

    create the App context provider and use it in TopDoctor component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import AppContextProvider from './context/AppContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <AppContextProvider>
     <App />
+    </AppContextProvider>
     </BrowserRouter>,
   
 );
