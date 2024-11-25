6f9a7e50b82ca46406057127f859c57d5ab5d1d2
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-11-25 22:28:46 +0500

    rearrange the files modify the paths format some part of the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,34 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Doctors from './pages/Doctors'
-import MyAppointments from './pages/MyAppointments'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import MyProfile from './pages/MyProfile'
-import Appointment from './pages/Appointment'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./PatientPages/Home";
+import Login from "./PatientPages/Login";
+import Doctors from "./PatientPages/Doctors";
+import MyAppointments from "./PatientPages/MyAppointments";
+import Contact from "./PatientPages/Contact";
+import About from "./PatientPages/About";
+import MyProfile from "./PatientPages/MyProfile";
+import Appointment from "./PatientPages/Appointment";
+import Navbar from "./PatientComponents/Navbar";
+import Footer from "./PatientComponents/Footer";
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
-      <Navbar/>
-     <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/doctors' element={<Doctors/>}></Route>
-      <Route path='/doctors/:speciality' element={<Doctors/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/contact' element={<Contact/>}></Route>
-      <Route path='/my-profile' element={<MyProfile/>}></Route>
-      <Route path='/my-appointments' element={<MyAppointments/>}></Route>
-      <Route path='/appointment/:docId' element={<Appointment/>}></Route>
-     
-     </Routes>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/doctors" element={<Doctors />}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/my-profile" element={<MyProfile />}></Route>
+        <Route path="/my-appointments" element={<MyAppointments />}></Route>
+        <Route path="/appointment/:docId" element={<Appointment />}></Route>
+      </Routes>
 
-     <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/PatientPages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../PatientComponents/Header";
+import SpecialityMenu from "../PatientComponents/SpecialityMenu";
+import TopDoctors from "../PatientComponents/TopDoctors";
+import Banner from "../PatientComponents/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
