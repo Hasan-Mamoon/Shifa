b807b38e1b9dbc0925db9794c6511cc5cab30c3c
Hassan Mamoon <mianhassan085@gmail.com>
2024-11-29 19:15:33 +0500

    Updated doctor-routes

--- backend/routes/doctor-routes.js
+++ backend/routes/doctor-routes.js
@@ -5,15 +5,15 @@ const router = express.Router();
 
 router.get("/doctors", async (req, res) => {
   try {
-    const applicantdata = await doctormodel.find({});
-    if (!applicantdata) {
+    const doctordata = await doctormodel.find({});
+    if (!doctordata) {
       return res.status(401).json({ message: "Invalid Email" });
     }
-    return res.json(applicantdata); 
+    return res.json(doctordata); 
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-export {router as applicantrouter}
+export {router as doctorRouter}
