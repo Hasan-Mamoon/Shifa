82ba9467840242bf56f0eaebdddc31f92d08d2e1
Ali Ammar Awan <aliammarawan11@gmail.com>
2024-12-08 15:05:04 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,20 +29,20 @@ import Profile from "./DoctorPages/Profile";
 const App = () => {
   return (
      <div className="mx-4 sm:mx-[10%]">
-    {/* //   <Navbar />
-    //   <Routes>
-    //     <Route path="/" element={<Home />}></Route>
-    //     <Route path="/doctors" element={<Doctors />}></Route>
-    //     <Route path="/doctors/:speciality" element={<Doctors />}></Route>
-    //     <Route path="/login" element={<Login />}></Route>
-    //     <Route path="/about" element={<About />}></Route>
-    //     <Route path="/contact" element={<Contact />}></Route>
-    //     <Route path="/my-profile" element={<MyProfile />}></Route>
-    //     <Route path="/my-appointments" element={<MyAppointments />}></Route>
-    //     <Route path="/appointment/:docId" element={<Appointment />}></Route>
-    //   </Routes>
+     {/* <Navbar />
+     <Routes>
+       <Route path="/" element={<Home />}></Route>
+       <Route path="/doctors" element={<Doctors />}></Route>
+       <Route path="/doctors/:speciality" element={<Doctors />}></Route>
+       <Route path="/login" element={<Login />}></Route>
+       <Route path="/about" element={<About />}></Route>
+       <Route path="/contact" element={<Contact />}></Route>
+       <Route path="/my-profile" element={<MyProfile />}></Route>
+       <Route path="/my-appointments" element={<MyAppointments />}></Route>
+       <Route path="/appointment/:docId" element={<Appointment />}></Route>
+      </Routes>
 
-    //   <Footer /> */}
+     <Footer /> */}
 
 
 
